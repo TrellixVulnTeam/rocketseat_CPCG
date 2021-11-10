085d102245933ddf5de62274d97851c36f735542
Guilherme Fernandez do Nascimento <guilherme.nascimento@soulisto.com.br>
2021-11-10 19:08:37 -0300

    aula http

--- ignite/reactjs/01_github_explorer/src/components/RepositoryList.jsx
+++ ignite/reactjs/01_github_explorer/src/components/RepositoryList.jsx
@@ -1,19 +1,29 @@
+import {useState, useEffect} from 'react';
+
 import { RepositoryItem } from "./RepositoryItem";
-const repository ={
-    name: 'Unform',
-    description: 'Forms in React',
-    link: 'https://github.com/unform/unform'
-}
+
+import '../styles/repositories.scss';
+ 
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([]);
+    
+    //cuidado para não colocar o segundo parametro = causa looping
+    useEffect(() => {
+        fetch('https://api.github.com/users/guilhermefn1011/repos')
+        .then(response => response.json())
+        .then(data => setRepositories(data))
+    }, []) 
+  
     return (
         <section className="Repository-List">
             <h1>Lista de repositórios</h1>
 
             <ul>
-             <RepositoryItem repository = {repository}/>
-             <RepositoryItem repository = {repository}/>
-             <RepositoryItem repository = {repository}/>
-             <RepositoryItem repository = {repository}/>
+                {repositories.map(repository =>{
+                    return  <RepositoryItem key={repository.name} //react usar key referencia com map
+                    repository = {repository}/>
+                } )}
+            
             </ul>
         </section>
     );
